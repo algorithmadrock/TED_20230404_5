2d3013697bde3e8221b37081233819b8faef3e5b
luiza <luizamfelix@gmail.com>
2023-04-15 13:49:27 -0300

    Atualização 15/04/2023

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -6,34 +6,31 @@
 
 package controller;
 
+import br.edu.fateczl.filas.Fila;
 import br.edu.fateczl.pilhas.Pilha;
 
 public class Controller {
-	String verso, reverso;
 	
-	public  boolean palindromo(String palavra) throws Exception {
-		versoreverso(palavra);
-		if (verso.equals(reverso)) {
+	public static boolean palindromo(String txt) throws Exception {
+		
+		Pilha arvalap = new Pilha();
+		Fila palavra = new Fila();
+		REverse(txt, palavra, arvalap);
+		
+		if(palavra.toString().equals(arvalap.toString())) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-	private  void versoreverso(String palavra) throws Exception {
-		 String[] letras = palavra.split("");
-		 Pilha word = new Pilha(), drow = new Pilha();
-		 int i = letras.length - 1;
-		
-		 for (String letra : letras) {
-			drow.push(letra);
+	
+	private static void REverse(String txt, Fila palavra, Pilha arvalap) throws Exception {
+		int size = txt.length();
+		for (int i = 0; i< size; i++) {
+			char letra = txt.charAt(i);
+			arvalap.push(letra);
+			palavra.insert(letra);
 		}
-		 reverso = drow.toString();
-		
-		while(i>=0) {
-			word.push(letras[i]);
-			i--;
-		}
-		 verso = word.toString();
 	}
 	
 	
--- src/view/Principal.java
+++ src/view/Principal.java
@@ -13,16 +13,16 @@ public class Principal {
 
 	public static void main(String[] args) throws Exception {
 		int confirmacao = 0;
-		Controller palindromos = new Controller();
+		Controller c = new Controller();
 		do {
 			String palavra = JOptionPane.showInputDialog("Insira uma palavra pra desobrir se é ou não palíndromo.");
-			if (palindromos.palindromo(palavra)) {
+			if (c.palindromo(palavra)) {
 				confirmacao = JOptionPane.showConfirmDialog(null,palavra + " é palíndroma! :) \nQuer testar outra palavra?");
 			} else {
 				confirmacao = JOptionPane.showConfirmDialog(null,palavra + " não é palíndroma! :( \nQuer testar outra palavra?");
 			}
 		} while (confirmacao != 1);
-
+		System.out.println("Programa finalizado com sucesso!");
 	}
 
 }
